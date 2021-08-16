32f3f93463fb7f975fb7bf6fea32fe4727105a6e
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-16 16:07:32 -0500

    Wrote orders.controller. It's still missing some validation

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -6,4 +6,112 @@ const orders = require(path.resolve("src/data/orders-data"));
 // Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
+/************************** Middleware Functions **************************/
+// Any request with a body needs to have valid data
+function hasDataFields(req, res, next) {
+  const { data } = req.body; // grab the data from request body
+
+  // body.data MUST have deliverTo, mobileNumber, status, and dishes properties
+  const requiredFields = ["deliverTo", "mobileNumber", "status", "dishes"];
+  for (const field of requiredFields)
+    if (!data[field])
+      return next({
+        status: 400,
+        message:
+          field === dishes
+            ? `Order must include a ${field}`
+            : `Order must include a dish`, //Message has to be dish singular, not plural like in template
+      });
+
+  const { deliverTo, mobileNumber, status, dishes } = data;
+  res.locals.newDish = { deliverTo, mobileNumber, status, dishes };
+  return next();
+}
+
+// Any order request with a body needs to have valid dishes array
+function validateDishes(req, res, next) {
+  const { dishes } = res.locals.newDish;
+
+  // dishes MUST be an array that is not empty
+  if (!Array.isArray(dishes) || !dishes.length)
+    return next({
+      status: 400,
+      message: `Order must include at least one dish`,
+    });
+
+  // Each dish in dishes needs to have a quantity and it must be a positive integer
+  dishes.forEach(({ quantity }, index) => {
+    if (!quantity || quantity < 0 || !Number.isInteger(quantity))
+      return next({
+        status: 400,
+        message: `Dish ${index} must have a quantity that is an integer greater than 0`,
+      });
+  });
+
+  return next();
+}
+
+function orderExists(req, res, next) {
+  const orderId = req.params.orderId; // grab the orderId from the request parameters
+  const foundOrder = orders.find((order) => order.id === orderId); // find an order that matches the retrieved id
+
+  // if there is no match, throw a 400 error
+  if (!foundOrder)
+    return next({ status: 404, message: `orderId ${orderId} does not exist` });
+  // else it does exist and we can save the foundOrder to res.locals
+  res.locals.foundOrder = foundOrder;
+  return next();
+}
+
+function bodyIdMatches(req, res, next) {
+  //If the request body specifies an id, it must match the id in the request url
+  const bodyId = res.locals.newOrder.id;
+  const routeId = res.locals.foundOrder.id;
+  if (bodyId && bodyId !== routeId)
+    return next({
+      status: 400,
+      message: `Order id does not match route id. Order: ${bodyId}, Route: ${routeId}`,
+    });
+  return next();
+}
+
+/********************************* L-CRUD *********************************/
+function list(req, res) {
+  // List all of the order objects as JSON
+  res.json({ data: orders });
+}
+
+function create(req, res) {
+  // Adds a unique ID to the new order object
+  res.locals.newOrder = { id: nextId(), ...res.locals.newOrder };
+
+  // Adds the newOrder object to the array of orders, and sends 201 response with the new object as JSON
+  dishes.push(res.locals.newOrder);
+  res.status(201).json({ data: res.locals.newOrder });
+}
+
+function read(req, res) {
+  // Read the found dish object as JSON
+  res.json({ data: res.locals.foundOrder });
+}
+
+function update(req, res) {
+  // Overwrite the foundOrder's data with the newOrder's data
+  res.locals.foundOrder = { ...res.locals.foundOrder, ...res.locals.newOrder };
+  res.json({ data: res.locals.foundOrder });
+}
+
+function destroy(req, res) {
+  // Find the index of the foundOrder within the orders array and remove it
+  const index = orders.indexOf(res.locals.foundOrder);
+  orders.splice(index, 1);
+  res.sendStatus(204);
+}
+
+module.exports = {
+  list: [list],
+  create: [hasDataFields, validateDishes, create],
+  read: [orderExists, read],
+  update: [orderExists, hasDataFields, validateDishes, bodyIdMatches, update],
+  delete: [orderExists, destroy],
+};
