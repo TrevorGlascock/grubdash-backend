1941b96a16f64432e8b32cc513d008de5011bee0
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-16 13:53:52 -0500

    Wrote orders.router

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,18 @@
 const router = require("express").Router();
+const controller = require("./orders.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-// TODO: Implement the /orders routes needed to make the tests pass
+router
+  .route("/:orderId")
+  .get(controller.read) // Read one specific order
+  .put(controller.update) // Update one specific order
+  .delete(controller.delete) // Delete one specific order
+  .all(methodNotAllowed); // All else not allowed
+
+router
+  .route("/")
+  .get(controller.list) // List all orders
+  .post(controller.create) // Create a new order
+  .all(methodNotAllowed); // All else not allowed
 
 module.exports = router;
