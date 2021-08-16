808c2715980566eb409517087d1b1664fc2308d7
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-16 12:49:58 -0500

    Wrote dishes.controller

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,3 +1,4 @@
+const { readSync } = require("fs");
 const path = require("path");
 
 // Use the existing dishes data
@@ -6,4 +7,75 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
+/************************** Middleware Functions **************************/
+function hasBodyFields(req, res, next) {
+  const data = req.body.data; // grab the data from request body
+
+  // body.data MUST have name, description, price, and image_url properties
+  const requiredFields = ["name", "description", "price", "image_url"];
+  for (const field of requiredFields)
+    if (!data[field])
+      return next({
+        status: 404,
+        message: `Dish must include a ${field}`,
+      });
+
+  // Save the new object to res.locals
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  res.locals.newDish = { name, description, price, image_url };
+  return next();
+}
+
+function priceIsValid(req, res, next) {
+  // Price cannot be less than 0
+  if (res.locals.newDish.price < 0)
+    return next({
+      status: 400,
+      message: `Dish must have a price that is an integer greater than 0`,
+    });
+  return next();
+}
+
+function dishExists(req, res, next) {
+  const dishId = req.params.dishId; // grab the dishId from the request parameters
+  const foundDish = dishes.find((dish) => dish.id === dishId); // find a dish that matches the retrieved id
+
+  // if there is no match, throw a 404 error
+  if (!foundDish)
+    return next({ status: 404, message: `dishId ${dishId} does not exist` });
+  // else it does exist and we can save the foundDish to res.locals
+  res.locals.foundDish = foundDish;
+  return next();
+}
+
+/********************************* L-CRUD *********************************/
+function list(req, res) {
+  // List all of the dish objects as JSON
+  res.json({ data: dishes });
+}
+
+function create(req, res) {
+  // Adds a unique ID to the new dish object
+  const newDish = { id: nextId(), ...req.locals.newDish };
+
+  // Adds the newDish object to the array of dishes, and sends 201 response with the new object as JSON
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+}
+
+function read(req, res) {
+  // Read the found dish object as JSON
+  res.json({ data: res.locals.foundDish });
+}
+
+function update(req, res) {
+  // Overwrite the found dish's data with the newDish's data
+  res.locals.foundDish = { ...res.locals.foundDish, ...res.locals.newDish };
+}
+
+module.exports = {
+  list: [list],
+  create: [hasBodyFields, priceIsValid, create],
+  read: [dishExists, read],
+  update: [dishExists, hasBodyFields, priceIsValid, update],
+};
