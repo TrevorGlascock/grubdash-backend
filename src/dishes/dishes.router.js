a8a3cc1154622c04305c9044637a47b172f9269d
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-16 13:05:42 -0500

    wrote dishes.router

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,17 @@
 const router = require("express").Router();
+const controller = require("./dishes.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-// TODO: Implement the /dishes routes needed to make the tests pass
+router
+  .route("/:dishId")
+  .get(controller.read) // Read one specific dish
+  .put(controller.update) // Update one specific dish
+  .all(methodNotAllowed); // All else not allowed
+
+router
+  .route("/")
+  .get(controller.list) // List all dishes
+  .post(controller.create) // Create one specific dish
+  .all(methodNotAllowed); // All else not allowed
 
 module.exports = router;
